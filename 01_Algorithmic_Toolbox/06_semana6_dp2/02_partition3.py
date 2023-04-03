b79125b980d1f9513e0d71730a9880be2b3a6acb
Gabriel Pinho <gabriel.augusto@petrobras.com.br>
2023-04-03 16:33:30 -0300

    partition3 e parenteses

--- 01_Algorithmic_Toolbox/06_semana6_dp2/02_partition3.py
+++ 01_Algorithmic_Toolbox/06_semana6_dp2/02_partition3.py
@@ -1,25 +1,25 @@
-# from sys import stdin
-# import numpy as np
+from sys import stdin
+import numpy as np
 
-# def partition3(values):
-#     if sum(values) % 3 != 0:
-#         return 0
-#     target = sum(values) // 3
-#     array = np.zeros((target+1,target+1))
-#     array[0][0] = 1
-#     n = len(values)
-#     for i in range(n):
-#         for j in range(target,-1,-1):
-#             for k in range(target,-1,-1):
-#                 if array[j][k] == 0:
-#                     value = j - values[i]
-#                     if value >= 0:
-#                         array[j][k] = array[value][k]
-#                 if array[j][k] == 0:
-#                     value = j - values[i]
-#                     if value >= 0:
-#                         array[j][k] = array[j][value]
-#     return array[target][target]
+def partition3(values):
+    if sum(values) % 3 != 0:
+        return 0
+    target = sum(values) // 3
+    array = np.zeros((target+1,target+1), dtype=bool)
+    array[0][0] = 1
+    n = len(values)
+    for i in range(n):
+        for j in range(target,-1,-1):
+            for k in range(target,-1,-1):
+                if array[j][k] == 0:
+                    value = j - values[i]
+                    if value >= 0:
+                        array[j][k] = array[value][k]
+                if array[j][k] == 0:
+                    value = j - values[i]
+                    if value >= 0:
+                        array[j][k] = array[j][value]
+    return array[target][target]
 
 
 # if __name__ == '__main__':
@@ -28,26 +28,27 @@
 #     assert input_n == len(input_values)
 #     print(int(partition3(input_values)))
 
-import numpy as np
+# import numpy as np
 
-def partition3(values):
-    if sum(values) % 3 != 0:
-        return 0
-    target = sum(values) // 3
-    array = np.zeros((target+1, target+1, len(values)+1), dtype=bool)
-    array[0][0][0] = True
-    for i in range(len(values)):
-        for j in range(target+1):
-            for k in range(target+1):
-                array[j][k][i+1] = array[j][k][i]
-                if j >= values[i]:
-                    array[j][k][i+1] |= array[j-values[i]][k][i]
-                if k >= values[i]:
-                    array[j][k][i+1] |= array[j][k-values[i]][i]
-    return array[target][target][len(values)]
+# def partition3(values):
+#     if sum(values) % 3 != 0:
+#         return 0
+#     target = sum(values) // 3
+#     array = np.zeros((target+1, target+1, len(values)+1), dtype=bool)
+#     array[0][0][0] = True
+#     for i in range(len(values)):
+#         for j in range(target+1):
+#             for k in range(target+1):
+#                 array[j][k][i+1] = array[j][k][i]
+#                 if j >= values[i]:
+#                     array[j][k][i+1] |= array[j-values[i]][k][i]
+#                 if k >= values[i]:
+#                     array[j][k][i+1] |= array[j][k-values[i]][i]
+#     return array[target][target][len(values)]
 
 
 if __name__ == '__main__':
-    input_n, *input_values = map(int, input().split())
-    assert input_n == len(input_values)
-    print(int(partition3(input_values)))
+    input_n, *values = list(map(int, stdin.read().split()))
+    # n = int(input())
+    # values = list(map(int, input().split()))
+    print(int(partition3(values)))
--- 01_Algorithmic_Toolbox/06_semana6_dp2/03_placing_parentheses.py
+++ 01_Algorithmic_Toolbox/06_semana6_dp2/03_placing_parentheses.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def evaluate(a, b, op):
     if op == '+':
         return a + b
@@ -9,10 +11,53 @@ def evaluate(a, b, op):
         assert False
 
 
-def maximum_value(dataset):
-    # write your code here
-    return 0
+# def maximum_value(num, op):
+#     n, inf = len(num), 9**9
+#     my_min = [[0] * n for _ in range(n)]
+#     my_max = [[0] * n for _ in range(n)]
+#     for i in range(n):
+#         my_min[i][i] = my_max[i][i] = int(num[i])
+#     for size in range(1,n):
+#         for i in range(n-size):
+#             j = i+size
+#             my_min[i][j], my_max[i][j] = inf, -inf
+#             for k in range(i,j):
+#                 value = evaluate(my_min[i][k], my_min[k+1][j], op[k])
+#                 my_min[i][j] = min(value, my_min[i][j])
+#                 my_max[i][j] = max(value, my_max[i][j])
+#                 value = evaluate(my_min[i][k], my_min[k+1][j], op[k])
+#                 my_min[i][j] = min(value, my_min[i][j])
+#                 my_max[i][j] = max(value, my_max[i][j])
+#                 value = evaluate(my_min[i][k], my_min[k+1][j], op[k])
+#                 my_min[i][j] = min(value, my_min[i][j])
+#                 my_max[i][j] = max(value, my_max[i][j])
+#                 value = evaluate(my_min[i][k], my_min[k+1][j], op[k])
+#                 my_min[i][j] = min(value, my_min[i][j])
+#                 my_max[i][j] = max(value, my_max[i][j])
+#     return my_max[0][n-1]
+
+def maximum_value(num, op):
+    n, inf = len(num), 9 ** 9
+    my_min = [[0] * n for _ in range(n)]
+    my_max = [[0] * n for _ in range(n)]
+    for i in range(n):
+        my_min[i][i] = my_max[i][i] = int(num[i])
+    for size in range(1, n):
+        for i in range(n - size):
+            j = i + size
+            my_min[i][j], my_max[i][j] = inf, -inf
+            value = evaluate(my_min[i][k], my_min[k + 1][j], op[k])
+            for k in range(i, j):
+                temp = evaluate(my_min[i][k], my_min[k + 1][j], op[k])
+                value = min(value, temp)
+                my_min[i][j] = min(my_min[i][j], temp, evaluate(my_min[i][k], my_max[k + 1][j], op[k]), evaluate(my_max[i][k], my_min[k + 1][j], op[k]))
+                my_max[i][j] = max(my_max[i][j], temp, evaluate(my_min[i][k], my_max[k + 1][j], op[k]), evaluate(my_max[i][k], my_min[k + 1][j], op[k]))
+    return my_max[0][n - 1]
 
 
 if __name__ == "__main__":
-    print(maximum_value(input()))
+    string = input()
+    numbers, operations = list(), list()
+    numbers = string[0:len(string):2]
+    operations = string[1:len(string)-1:2]
+    print(int(maximum_value(numbers,operations)))
