713924dd5e80a221374223d58c649e35451dc2f6
Gabriel Pinho <gabriel.augusto@petrobras.com.br>
2023-04-14 14:37:44 -0300

    data structures semana 3

---
+++ 02_data_structures/03_semana3_priority_queues_disj_sets/01_build_heap.py
@@ -0,0 +1,67 @@
+# python3
+
+#zero index:
+#    0
+#  1   2
+# 3 4 5 6
+
+
+# pseudocode from lecture
+#adaptado para zero indexing
+def parent(i):
+    return (i-1)//2
+def leftChild(i):
+    return 2*i + 1
+def rightChild(i):
+    return 2*i + 2
+
+def shiftUp(H,i):
+    while i > 0 and H[parent(i)] < H[i]:
+        H[parent(i)], H[i] = H[i], H[parent(i)]
+        i = parent(i)
+    return H
+
+def shiftDown(H,i):
+    maxIndex = i
+    l = leftChild(i)
+    if H[l] > H[maxIndex]:
+        maxIndex = l
+    if H[r] > H[maxIndex]:
+        maxIndex = r
+    if i != maxIndex:
+        H[i], H[maxIndex] = H[maxIndex], H[i]
+        shiftDown(maxIndex)
+def build_heap(data):
+    """Build a heap from ``data`` inplace.
+
+    Returns a sequence of swaps performed by the algorithm.
+    """
+    # The following naive implementation just sorts the given sequence
+    # using selection sort algorithm and saves the resulting sequence
+    # of swaps. This turns the given array into a heap, but in the worst
+    # case gives a quadratic number of swaps.
+    #
+    # TODO: replace by a more efficient implementation
+    swaps = []
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            if data[i] > data[j]:
+                swaps.append((i, j))
+                data[i], data[j] = data[j], data[i]
+    return swaps
+
+
+def main():
+    n = int(input())
+    data = list(map(int, input().split()))
+    assert len(data) == n
+
+    swaps = build_heap(data)
+
+    print(len(swaps))
+    for i, j in swaps:
+        print(i, j)
+
+
+if __name__ == "__main__":
+    main()
